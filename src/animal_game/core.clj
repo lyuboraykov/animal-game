0709ac9f2b4d0da4a78f096e1b2be00f325e716e
Lyubomir Raykov <raykovl@uber.com>
2015-12-30 19:10:32 +0200

    Adding sample data files

--- src/animal_game/core.clj
+++ src/animal_game/core.clj
@@ -1,12 +1,31 @@
 (ns animal-game.core
   (:gen-class))
 
+(defn animal-found? [] false)
+
+(def root-question "Is it a cat?")
+
+(defn save-answer
+    "Save the answer of the question in the JSON file"
+    [decision-tree question answer]
+    (println "Answer saved!"))
+
+(defn next-question
+  "Get the next question to be asked"
+  [decision-tree]
+  root-question)
+
+(defn ask-question
+  "Ask the user to provide an aswer for a question."
+  [decision-tree]
+  (while (not (animal-found?))
+    (let [question (next-question decision-tree)](
+      (println question)
+      (let [answer (read-line)]
+      (save-answer decision-tree question answer))))))
+
+
 (defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Is it a dog?")
-  (let [input (read-line)]
-    (if (or (= input "y") (= input "yes"))
-    (println "It's a dog")
-    (println "It's not a dog")
-    )))
+    "Simple user input and output."
+    [& args]
+    (ask-question {}))
