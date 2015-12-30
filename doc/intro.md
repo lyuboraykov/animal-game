b516955f30395e1eedfe917e073a4ec4b68b1efd
lyuboraykov <lyubo.raykov@gmail.com>
2015-12-30 12:24:18 +0200

    Initial project commit with leiningen bootstrap

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to animal-game
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ src/animal_game/core.clj
@@ -0,0 +1,7 @@
+(ns animal-game.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
