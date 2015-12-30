3efcc31fade1ed2e2cb4c0ba124eadd3854c7d7d
Lyubomir Raykov <raykovl@uber.com>
2015-12-30 15:57:11 +0200

    Simple user input and output

--- src/animal_game/core.clj
+++ src/animal_game/core.clj
@@ -4,4 +4,9 @@
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (println "Is it a dog?")
+  (let [input (read-line)]
+    (if (or (= input "y") (= input "yes"))
+    (println "It's a dog")
+    (println "It's not a dog")
+    )))
