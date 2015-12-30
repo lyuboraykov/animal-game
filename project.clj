da7a3100096c8be89de745ceaff189f4377ca712
Lyubomir Raykov <raykovl@uber.com>
2015-12-30 12:59:37 +0200

    Change description and license in project.clj

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject animal-game "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :description "Simple decision-tree learning game"
+  :url "https://github.com/lyuboraykov/animal-game"
+  :license {:name "MIT License"
+            :url "https://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.7.0"]]
   :main ^:skip-aot animal-game.core
   :target-path "target/%s"
